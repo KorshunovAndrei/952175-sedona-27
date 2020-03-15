f2f3335c81c0b1af4b036f14dda0f962955ae238
Коршунов Андрей <60074147+KorshunovAndrei@users.noreply.github.com>
2020-03-16 02:45:24 +0400

    Правки

--- js/script.js
+++ js/script.js
@@ -1,26 +1,24 @@
 "use strict";
 
-let saerch = document.querySelector(".search-button");
+let search = document.querySelector(".search-button");
 let form = document.querySelector(".search-form form");
-let arrival_date = form.querySelector("[name=arrival-date]");
-let date_of_departure = form.querySelector("[name=date-of-departure]");
+let formBtn = form.querySelector(".form-button");
+let arrivalDate = form.querySelector("[name=arrival-date]");
+let dateOfDeparture = form.querySelector("[name=date-of-departure]");
 let adult = form.querySelector("[name=adult-number]");
 let children = form.querySelector("[name=children-number]");
 let map = document.querySelector(".map");
 
-saerch.addEventListener("click", function (evt) {
+search.addEventListener("click", function (evt) {
 	evt.preventDefault();
-	if(form.classList.contains("search-form-on")){
-		form.classList.remove("search-form-on");
+	if(!form.classList.contains("search-form-off")) 
 		form.classList.add("search-form-off");
-	}	else {
+	else
 		form.classList.remove("search-form-off");
-		form.classList.add("search-form-on");
-	}
 });
 
 form.addEventListener("submit", function(evt){
-	if(!arrival_date.value || !date_of_departure.value || !adult.value || !children.value) {
+	if(!arrivalDate.value || !dateOfDeparture.value || !adult.value || !children.value) {
 		evt.preventDefault();
 	}
 });
