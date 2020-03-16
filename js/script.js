78e65dea07a26bad880f9da580567d19263f7b9d
Коршунов Андрей <60074147+KorshunovAndrei@users.noreply.github.com>
2020-03-16 18:06:57 +0400

    Очередные правки

--- js/script.js
+++ js/script.js
@@ -2,19 +2,20 @@
 
 let searchBtn = document.querySelector(".search-button");
 let form = document.querySelector(".search-form form");
-let formBtn = form.querySelector(".form-button");
 let arrivalDate = form.querySelector("[name=arrival-date]");
 let dateOfDeparture = form.querySelector("[name=date-of-departure]");
 let adult = form.querySelector("[name=adult-number]");
 let children = form.querySelector("[name=children-number]");
 let map = document.querySelector(".map");
 
+form.classList.add("search-form-off");
+
 searchBtn.addEventListener("click", function (evt) {
 	evt.preventDefault();
-	if(!form.classList.contains("search-form-off")) 
-		form.classList.add("search-form-off");
-	else
+	if(form.classList.contains("search-form-off")) 
 		form.classList.remove("search-form-off");
+	else
+		form.classList.add("search-form-off");
 });
 
 form.addEventListener("submit", function(evt){
